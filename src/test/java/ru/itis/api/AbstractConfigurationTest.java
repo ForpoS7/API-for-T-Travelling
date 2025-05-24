b3f384d7364def6ec7e92074d26799e1e103e3c5
ebzh <nanobot559955@mail.ru>
2025-05-24 21:38:56 +0300

    edited test configure

--- src/test/java/ru/itis/api/AbstractConfigurationTest.java
+++ src/test/java/ru/itis/api/AbstractConfigurationTest.java
@@ -1,20 +1,11 @@
 package ru.itis.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
 @SpringBootTest
@@ -30,18 +21,5 @@ public abstract class AbstractConfigurationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
-    }
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Test
-    void testDatabaseConnection() throws Exception {
-        try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT 1")) {
-
-            assertTrue(resultSet.next());
-        }
     }
 }
--- src/test/java/ru/itis/api/ApiForTTravellingApplicationTests.java
+++ src/test/java/ru/itis/api/ApiForTTravellingApplicationTests.java
@@ -1,20 +1,6 @@
 package ru.itis.api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ApiForTTravellingApplicationTests extends AbstractConfigurationTest {
 
