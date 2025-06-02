13d353a3a913d64c840d643b30341b6ee2456f55
Kadim Arslanov <144515768+RyanGoslingIsNotDead@users.noreply.github.com>
2025-06-02 19:47:09 +0300

    adjustment enum Category

--- src/main/java/ru/itis/api/dictionary/Category.java
+++ src/main/java/ru/itis/api/dictionary/Category.java
@@ -4,21 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Enumeration of possible transaction categories")
 public enum Category {
-    @Schema(name = "TICKETS", description = "Transportation tickets (flights, trains, etc.)")
-    TICKETS,
-
-    @Schema(name = "HOTELS", description = "Hotel or accommodation expenses")
-    HOTELS,
-
-    @Schema(name = "FOOD", description = "Food and dining expenses")
+    TRANSPORT,
+    ACCOMMODATION,
     FOOD,
-
-    @Schema(name = "ENTERTAINMENT", description = "Activities, tours, sightseeing")
     ENTERTAINMENT,
-
-    @Schema(name = "INSURANCE", description = "Travel insurance costs")
-    INSURANCE,
-
-    @Schema(name = "OTHERS_EXPENSES", description = "Other miscellaneous expenses")
-    OTHERS_EXPENSES
+    SHOPPING,
+    HEALTH,
+    COMMUNICATION,
+    VISA_DOCUMENTS,
+    OTHER
 }
