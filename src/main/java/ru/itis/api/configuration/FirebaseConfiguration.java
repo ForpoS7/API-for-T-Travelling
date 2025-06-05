fa49652f8f45eef3dea927dfc10e5a7fa5469972
ebzh <nanobot559955@mail.ru>
2025-06-06 02:10:39 +0300

    fix deploy

--- src/main/java/ru/itis/api/configuration/FirebaseConfiguration.java
+++ src/main/java/ru/itis/api/configuration/FirebaseConfiguration.java
@@ -4,30 +4,19 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfiguration {
-    @Value("${firebase.config.json}")
-    private String firebaseConfigJson;
-
     @PostConstruct
     public void initialize() throws IOException {
-//        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-service-account.json");
-
-//        FirebaseOptions options = FirebaseOptions.builder()
-//                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                .build();
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-service-account.json");
 
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(
-                        new ByteArrayInputStream(firebaseConfigJson.getBytes())
-                ))
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
--- src/test/java/ru/itis/api/AbstractConfigurationTest.java
+++ src/test/java/ru/itis/api/AbstractConfigurationTest.java
@@ -3,13 +3,18 @@ package ru.itis.api;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import ru.itis.api.configuration.FirebaseConfiguration;
 
 @Testcontainers
 @SpringBootTest
 public abstract class AbstractConfigurationTest {
+    @MockitoBean
+    private FirebaseConfiguration firebaseConfiguration;
+
     @Container
     public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14")
             .withDatabaseName("testdb")
