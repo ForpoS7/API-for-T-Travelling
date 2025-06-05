76a43ee3920cf422a276a28edc0e94cfd397880b
ebzh <nanobot559955@mail.ru>
2025-06-05 22:29:47 +0300

    edited firebase config

--- src/main/java/ru/itis/api/configuration/FirebaseConfiguration.java
+++ src/main/java/ru/itis/api/configuration/FirebaseConfiguration.java
@@ -4,19 +4,23 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfiguration {
+    @Value("${firebase.config.json}")
+    private String firebaseConfigJson;
+
     @PostConstruct
     public void initialize() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-service-account.json");
-
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(
+                        new ByteArrayInputStream(firebaseConfigJson.getBytes())
+                ))
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
